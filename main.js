6332e68825befb437cc0ee9bfd7f45715aea87e5
Alexandre Kings <alexandrekings1@gmail.com>
2024-07-22 18:19:40 +0200

    Change CTX by SVG.

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const ctx = document.getElementById("canvas").getContext("2d");
+// const ctx = document.getElementById("canvas").getContext("2d");
 const sizeInput = document.getElementById("sizeInput")
 const zoomInput = document.getElementById("zoomInput")
 const container = document.getElementById("container")
@@ -7,6 +7,7 @@ const backColour = "lightGreen";
 const offColour = "lightGray";
 const onColour = "blue";
 const circleColour = "red";
+const svg = document.getElementById("svg");
 
 // Parameters
 let zoom = 1;
@@ -39,42 +40,47 @@ function plotGrid() {
     }
 
     // Draw on canvas
-    ctx.canvas.width = zoom * container.clientWidth;
-    ctx.canvas.height = zoom* container.clientHeight;
+    // ctx.canvas.width = zoom * container.clientWidth;
+    // ctx.canvas.height = zoom* container.clientHeight;
 
-    ctx.fillStyle = backColour;
-    ctx.fillRect(0,0,ctx.canvas.width, ctx.canvas.height);
+    // ctx.fillStyle = backColour;
+    // ctx.fillRect(0,0,ctx.canvas.width, ctx.canvas.height);
+
+    svg.innerHTML = ""
     
     nx = grid.length;
     ny = grid[0].length;
 
-    sx = ctx.canvas.width / nx;
-    sy = ctx.canvas.height / ny;
+    // sx = ctx.canvas.width / nx;
+    // sy = ctx.canvas.height / ny;
+
+    sx = 100/nx;
+    sy = 100/ny;
 
     for(x = 0; x < nx; x++) {
         for(y = 0; y < ny; y++) {
-            ctx.fillStyle = grid[x][y] ? onColour : offColour;
-            ctx.fillRect((x+borderSize) * sx, (y+borderSize) * sy, (1-2*borderSize)*sx, (1-2*borderSize)*sy);
+            // ctx.fillStyle = grid[x][y] ? onColour : offColour;
+            // ctx.fillRect((x+borderSize) * sx, (y+borderSize) * sy, (1-2*borderSize)*sx, (1-2*borderSize)*sy);
+            
+            // Create rectangle
+            let svgns = "http://www.w3.org/2000/svg";
+            let rect = document.createElementNS(svgns, 'rect');
+            rect.setAttribute("x", (x+borderSize) * sx);
+            rect.setAttribute("y", (y+borderSize) * sy);
+            rect.setAttribute("width", (1-2*borderSize)*sx);
+            rect.setAttribute("height", (1-2*borderSize)*sy);
+            rect.setAttribute("rx",0);
+            rect.setAttribute("ry",0);
+            rect.setAttribute("fill", grid[x][y] ? onColour : offColour);
+            svg.appendChild(rect);
         }
-
     }
     
-    // // Draw on canvas immediately
-    // sx = ctx.canvas.width / side;
-    // sy = ctx.canvas.height / side;
+    // Draw on SVG
     
-    // s = side/2;
-    // for(i = 0; i < side; i++) {
-    //     x = i-s;
-    //     ax = Math.abs(x + 0.5);
-    //     for(j = 0; j < side; j++) {
-    //         y = j-s;
-    //         ay = Math.abs(y + 0.5);
-    //         ctx.fillStyle = radius < Math.sqrt((ax + 0.5)**2 + (ay+0.5)**2) && radius > Math.sqrt((ax-0.501)**2 + (ay - 0.501)**2) ? onColour : offColour;
-            
-    //         ctx.fillRect((i+borderSize) * sx, (j+borderSize) * sy, (1-2*borderSize)*sx, (1-2*borderSize)*sy);
-    //     }
-    // }
+
+
+
 }
 
 sizeInput.addEventListener("change", ()=> {
@@ -88,7 +94,7 @@ sizeInput.addEventListener("change", ()=> {
 timeId = 0;
 window.addEventListener("resize", ()=>{
     clearTimeout(timeId);
-    timeId = setTimeout(()=>{plotGrid(r)}, 200);
+    timeId = setTimeout(()=>{plotGrid()}, 200);
 })
 
 timeId2 = 0;

c52bd15db38d739a4cf1eff249d1a5aa8755d80f
Alexandre Kings <alexandrekings1@gmail.com>
2024-07-22 16:19:33 +0200

    Add zoom.

--- main.js
+++ main.js
@@ -1,17 +1,27 @@
 const ctx = document.getElementById("canvas").getContext("2d");
 const sizeInput = document.getElementById("sizeInput")
+const zoomInput = document.getElementById("zoomInput")
+const container = document.getElementById("container")
 const borderSize = 0.05;
 const backColour = "lightGreen";
 const offColour = "lightGray";
 const onColour = "blue";
 const circleColour = "red";
 
-function plotGrid(side) {
-    start = window.performance.now()
+// Parameters
+let zoom = 1;
+let side = 4;
+sizeInput.value = side;
+zoomInput.value = 0;
 
+// Run on start
+plotGrid();
+
+
+function plotGrid() {
     // Initialise canvas
-    ctx.canvas.width = ctx.canvas.offsetWidth;
-    ctx.canvas.height = ctx.canvas.offsetHeight;
+    ctx.canvas.width = zoom * container.offsetWidth;
+    ctx.canvas.height = zoom* container.offsetHeight;
 
     ctx.fillStyle = backColour;
     ctx.fillRect(0,0,ctx.canvas.width, ctx.canvas.height);
@@ -32,23 +42,15 @@ function plotGrid(side) {
             ctx.fillStyle = radius < Math.sqrt((ax + 0.5)**2 + (ay+0.5)**2) && radius > Math.sqrt((ax-0.501)**2 + (ay - 0.501)**2) ? onColour : offColour;
             
             ctx.fillRect((i+borderSize) * sx, (j+borderSize) * sy, (1-2*borderSize)*sx, (1-2*borderSize)*sy);
-
         }
     }
-
-    end = window.performance.now();
-    console.log("elapsed = ", end - start)
 }
-
-// Run on start
-r = 4;
-sizeInput.value = r;
-plotGrid(r);
 
 sizeInput.addEventListener("change", ()=> {
     r = parseInt(sizeInput.value);
     if(r < 2 || r > 400) return;
-    plotGrid(r)
+    side = r
+    plotGrid()
 })
 
 // On resize of window
@@ -57,3 +59,11 @@ window.addEventListener("resize", ()=>{
     clearTimeout(timeId);
     timeId = setTimeout(()=>{plotGrid(r)}, 200);
 })
+
+timeId2 = 0;
+zoomInput.oninput = ()=>{
+    clearTimeout(timeId2);
+    zoom = 0.1 * Number(zoomInput.value) + 1;
+    timeId2 = setTimeout(()=>plotGrid(), 10);
+    
+}
